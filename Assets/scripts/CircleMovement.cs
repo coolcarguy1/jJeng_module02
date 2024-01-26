2353b0a9fb2d4c1b3b999b8d2b45990443326d37
Lars Pedersen <123490179+marsbarslars@users.noreply.github.com>
2024-01-26 16:50:31 -0600

    Lars's initial changes

---
+++ Assets/scripts/CircleMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CircleMovement : MonoBehaviour
+{
+    public float r;
+    public float speed;
+    private float x;
+    private float z;
+    private float theta = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Increment theta but keep it below 2pi
+        theta += 0.1f * speed * Time.deltaTime;
+        if (theta > 2f * Mathf.PI)
+        {
+            theta -= 2f * Mathf.PI;
+        }
+
+        x = r * Mathf.Cos(theta);
+        z = r * Mathf.Sin(theta);
+
+        transform.position = new Vector3(x, 0.5f, z);
+    }
+}
